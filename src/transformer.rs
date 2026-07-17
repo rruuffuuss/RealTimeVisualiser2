d051d3308e36a027bbb29ce9d65281377ee1fc22
rufus <mail@rufusponniah.com>
2026-07-17 20:37:43 +0100

    add transform_split function for samples split across 2 slices (used with VecDeque non contiguous output)

--- src/transformer.rs
+++ src/transformer.rs
@@ -1,5 +1,5 @@
 use rustfft::{Fft, FftPlanner, num_complex::Complex};
-use std::{process::Output, sync::Arc};
+use std::sync::Arc;
 
 pub struct Transformer {
     input_samples: usize,
@@ -25,19 +25,38 @@ impl Transformer {
         }
     }
 
+    /// same as transform but supports split slices as output from a VecDeque
+    pub fn transform_split(&mut self, input1: &[f32], input2: &[f32]) -> Vec<f32> {
+        //copy the input into the real part of complex numbers in the input buffer
+        self.input_buffer
+            .iter_mut()
+            .zip(input1.iter().chain(input2.iter()))
+            .for_each(|(b, i)| *b = Complex::new(*i, 0.0));
+
+        self._transform_inner()
+    }
+
     pub fn transform(&mut self, input: &[f32]) -> Vec<f32> {
         // copy the input into the real part of complex numbers in the input buffer
-        for i in 0..self.input_samples {
-            self.input_buffer[i] = Complex::new(input[i], 0.0)
-        }
+        self.input_buffer
+            .iter_mut()
+            .zip(input.iter())
+            .for_each(|(b, i)| *b = Complex::new(*i, 0.0));
 
+        self._transform_inner()
+    }
+
+    pub fn _transform_inner(&mut self) -> Vec<f32> {
         self.fft.process(&mut self.input_buffer);
 
         // copy the real part of the transform into the output
         // only half is copied since fourier transform of real only input is a mirrored
+        // could potentially add other bar merging options like max or a weighted average
         self.input_buffer[..self.input_samples / 2]
             .chunks(self.bins_per_bar)
-            .map(|bins| bins.iter().map(|bin| bin.norm_sqr()).sum::<f32>() / bins.len() as f32)
+            .map(|bins| {
+                bins.iter().map(|bin| bin.norm_sqr()).sum::<f32>() / self.bins_per_bar as f32
+            })
             .collect()
     }
 }
@@ -56,6 +75,34 @@ mod tests {
         let mut t = Transformer::new(input.len(), input.len() / 2);
 
         let result = t.transform(&input);
+
+        print!("{:?}", result);
+
+        let max_index = result
+            .iter()
+            .enumerate()
+            .max_by(|(_, a), (_, b)| a.total_cmp(b))
+            .map(|(index, _)| index)
+            .unwrap();
+
+        assert_eq!(7_usize, max_index)
+    }
+
+    #[test]
+    fn test_single_frequency_7_hz_split_slices() {
+        let input1 = vec![0.0_f32, 1.0, 0.0, -1.0, 0.0, 1.0, 0.0, -1.0, 0.0, 1.0, 0.0];
+
+        let input2 = vec![
+            -1.0, 0.0, 1.0, 0.0, -1.0, 0.0, 1.0, 0.0, -1.0, 0.0, 1.0, 0.0, -1.0, 0.0, 1.0, 0.0,
+            -1.0, 0.0,
+        ];
+
+        let mut t = Transformer::new(
+            input1.len() + input2.len(),
+            (input1.len() + input2.len()) / 2,
+        );
+
+        let result = t.transform_split(&input1, &input2);
 
         print!("{:?}", result);
 
